c9e69c42b990c802207226c7196da25392428626
jacxdev <jacamachoc@outlook.com>
2020-01-16 15:49:39 -0600

    Se agrega codigo fuente

---
+++ src/components/Formulario.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Formulario = () => {
+    return (
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form className="col card text-white bg-transparent mb-5 pt-5 pb-2" action="">
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Formulario
